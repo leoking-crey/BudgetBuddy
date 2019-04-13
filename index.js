8b4c3142af27583582683d523da5c260adc34bd1
Ubuntu <ubuntu@ip-172-31-22-96.us-east-2.compute.internal>
2019-04-13 14:50:13 +0000

    fix

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const store = require('./store')
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.post('/expenseSave', (req, res) => {
    console.log("saving data")
-})
-
-app.get('/', (req, res) => {
-  res.send('HEY!')
 })
 app.listen(3001, () => console.log('Server running on port 3001'))
