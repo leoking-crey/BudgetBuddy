c687938f4d4f9731bb54ee9fc2e5c8d1877fc81a
Ubuntu <ubuntu@ip-172-31-22-96.us-east-2.compute.internal>
2019-04-13 16:15:41 +0000

    enabled cors external requests

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(express.static('public'))
 app.use(bodyParser.json())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.post('/expenseSave', (req, res) => {
    console.log("saving data")
 })
