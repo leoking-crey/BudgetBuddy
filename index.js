c994caaea34b1ef1863ab177e331757db23a99dc
Ubuntu <ubuntu@ip-172-31-22-96.us-east-2.compute.internal>
2019-04-13 14:45:22 +0000

    added post

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const store = require('./store')
 const app = express()
+app.use(express.static('public'))
+app.use(bodyParser.json())
+app.post('/expenseSave', (req, res) => {
+   console.log("saving data")
+})
+
 app.get('/', (req, res) => {
   res.send('HEY!')
 })
